db0e28dc987a65cbe490e0ac63dff81e6eed4c8b
Courtlyn Boykin <courtlynb@google.com>
2024-01-16 17:52:48 -0500

    added strings and drawables, created Recommendation data class, and category enum class

---
+++ app/src/main/java/com/example/atlanta/data/Recommendation.kt
@@ -0,0 +1,11 @@
+package com.example.atlanta.data
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Recommendation (
+    val category: Category,
+    @StringRes val name: Int,
+    @StringRes val description: Int,
+    @DrawableRes val avatar: Int,
+)
