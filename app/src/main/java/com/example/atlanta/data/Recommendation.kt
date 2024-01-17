ea172fac75caab02b0d437d288b0dfcc318de881
Courtlyn Boykin <courtlynb@google.com>
2024-01-17 11:36:57 -0500

    added LocalRecommendationsDataProvider

--- app/src/main/java/com/example/atlanta/data/Recommendation.kt
+++ app/src/main/java/com/example/atlanta/data/Recommendation.kt
@@ -3,7 +3,7 @@ package com.example.atlanta.data
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
-data class Recommendation (
+data class Recommendation(
     val category: Category,
     @StringRes val name: Int,
     @StringRes val description: Int,
