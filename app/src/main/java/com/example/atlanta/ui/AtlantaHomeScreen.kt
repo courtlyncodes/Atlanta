15b16a90c14c098225c20a61b8dea1025d85e491
Courtlyn Boykin <courtlynb@google.com>
2024-01-18 11:34:05 -0500

    Created a top app bar and home screen composable for a basic home screen

--- app/src/main/java/com/example/atlanta/ui/AtlantaHomeScreen.kt
+++ app/src/main/java/com/example/atlanta/ui/AtlantaHomeScreen.kt
@@ -2,10 +2,17 @@ package com.example.atlanta.ui
 
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -17,32 +24,56 @@ import com.example.atlanta.data.local.LocalCategoryDataProvider
 import com.example.atlanta.ui.theme.AtlantaTheme
 
 @Composable
-fun CategoryCard(category: Category, modifier: Modifier = Modifier) {
-        Card {
-            Row {
-                Image(
-                    painter = painterResource(category.icon),
-                    contentDescription = category.name + " icon",
-                    modifier = modifier.size(32.dp)
-                )
-                Text(text = category.name)
-            }
+fun HomeScreen(categories: List<Category>) {
+    Scaffold(topBar = {
+        TopAppBar()
+    }
+    ) {
+//        val categoryList = LocalCategoryDataProvider.allCategories
+        Column( modifier = Modifier.padding(it)) {
+            CategoryList(categories = categories)
+        }
     }
 }
 
 @Composable
-fun CategoryList(categories: List<Category>, modifier: Modifier = Modifier){
-    Column {
-        categories.forEach { category ->
-            CategoryCard(category = category, modifier = Modifier)
+fun CategoryCard(category: Category, modifier: Modifier = Modifier) {
+    Card(modifier = Modifier.clickable { /*todo*/ }) {
+        Row(modifier = modifier.padding(horizontal = 10.dp, vertical = 10.dp)) {
+            Image(
+                painter = painterResource(category.icon),
+                contentDescription = category.name + " icon",
+                modifier = modifier.size(32.dp)
+            )
+            Spacer(modifier = modifier.width(20.dp))
+            Text(text = category.name)
         }
     }
 }
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(text = "Welcome to Atlanta")
+        }
+    )
+}
+
+@Composable
+fun CategoryList(categories: List<Category>, modifier: Modifier = Modifier) {
+            categories.forEach { category ->
+                CategoryCard(category = category, modifier = Modifier)
+            }
+    }
+
 @Preview(showBackground = true)
 @Composable
-fun CategoryCardPreview() {
+fun HomeScreenPreview() {
     AtlantaTheme {
         val categoryList = LocalCategoryDataProvider.allCategories
-        CategoryList(categories = categoryList, modifier = Modifier)
+        HomeScreen(categories = categoryList)
     }
 }
